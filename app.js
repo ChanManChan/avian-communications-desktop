5ab9d80e2f58dd18abfafcc828dc2ad8154f7900
NandaGopal <ngopal253@gmail.com>
2021-09-30 22:47:00 +0530

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const middleware = require('./middleware')
+const path = require("path")
+
+const server = app.listen(port, () => console.log(`Server listening on port ${port}`))
+
+app.set("view engine", "pug")
+app.set("views", "views")
+app.use(express.static(path.join(__dirname, "public")))
+
+// Routes
+const loginRoute = require('./routes/loginRoutes')
+const registerRoute = require('./routes/registerRoutes')
+app.use("/login", loginRoute)
+app.use("/register", registerRoute)
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
+  const payload = {
+    pageTitle: "Home"
+  }
+  res.status(200).render("home", payload)
+})
